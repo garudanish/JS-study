5477d719ec394872ae9b59693808a9bce06eb92f
HyeonsuKim <pkj3186@naver.com>
2021-01-08 19:44:45 +0900

    Complete weather part

--- momentum-clone/weather.js
+++ momentum-clone/weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "6e6e3c9b6582652b457c934014cb1f6d";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature}°C @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +29,7 @@ function handleGeoSucces(position) {
     longitude: longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,7 +45,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    // get weather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
