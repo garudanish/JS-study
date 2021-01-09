16cf936206334bb2b086248bbe9e61061c53b023
HyeonsuKim <pkj3186@naver.com>
2021-01-09 14:51:36 +0900

    Modify CSS

--- momentum-clone/weather.js
+++ momentum-clone/weather.js
@@ -13,7 +13,7 @@ function getWeather(lat, lng) {
     .then(function (json) {
       const temperature = json.main.temp;
       const place = json.name;
-      weather.innerText = `${temperature}°C @ ${place}`;
+      weather.innerHTML = `${temperature}°C<br>${place}`;
     });
 }
 
